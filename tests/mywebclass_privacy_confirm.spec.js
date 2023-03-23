bc882f31af5d79371d967457bca194c55304f04c
CFountain23 <90641672+CFountain23@users.noreply.github.com>
2023-03-22 22:17:57 -0400

    Privacy confirm playwright test

--- tests/mywebclass_privacy_confirm.spec.js
+++ tests/mywebclass_privacy_confirm.spec.js
@@ -2,18 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 const BASE_URL = 'http://localhost:3000';
 
-test('Verify user confirmation for privacy policy', async ({ page }) => {
-  // Navigate to the privacy policy page
-  await page.goto(`${BASE_URL}/privacy-policy`);
+test('Verify privacy policy pop-up', async ({ page }) => {
+  // Navigate to the home page
+  await page.goto(BASE_URL);
   await page.waitForLoadState();
 
-  // Check that the user confirmation dialog is present
-  const confirmationDialog = await page.$('.confirmation-dialog');
-  expect(confirmationDialog).toBeTruthy();
-
-  // Confirm the privacy policy
-  await confirmationDialog?.click('button.confirm');
-
-  // Check that the dialog has been dismissed
-  await page.waitForSelector('.confirmation-dialog', { state: 'hidden' });
+  // Check that the privacy policy pop-up is present
+  const privacyPolicyPopUp = await page.$('.modal-content');
+  expect(privacyPolicyPopUp).toBeTruthy();
 });
